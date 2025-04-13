92bb48974e9da39cacf950acc01234a6f8a3357a
Mofei1023 <71375565+Mofei1023@users.noreply.github.com>
2025-04-14 04:59:22 +0800

    about page update

--- frontend/src/pages/about.jsx
+++ frontend/src/pages/about.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import profile01 from './images/profile01.jpg';
+import profile02 from './images/profile02.jpg';
+import profile03 from './images/profile03.jpg';
+import profile04 from './images/profile04.jpg';
 import '../index.css';
-import profile01 from './images/alpaca_pic.jpg';
 
 class ChangeImg extends React.Component {
   constructor(props) {
@@ -11,68 +13,72 @@ class ChangeImg extends React.Component {
       currentImage: 0,
       images: [
         profile01,
+        profile02,
+        profile03,
+        profile04
       ]
     };
   }
 
   switchImage() {
-    if (this.state.currentImage < this.state.images.length - 1) {
-      this.setState({
-        currentImage: this.state.currentImage + 1
-      });
-    } else {
-      this.setState({
-        currentImage: 0
-      });
-    }
-    return this.currentImage;
+    this.setState((prevState) => ({
+      currentImage:
+        prevState.currentImage < this.state.images.length - 1
+          ? prevState.currentImage + 1
+          : 0,
+    }));
   }
 
   componentDidMount() {
-    setInterval(this.switchImage, 2500);
+    this.interval = setInterval(this.switchImage, 2500);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
   }
 
   render() {
     return (
-      <div class="col col1">
+      <div className="col col1">
         <img
-          class="image round fit"
+          className="image round fit"
           src={this.state.images[this.state.currentImage]}
-          alt=""
+          alt="Profile"
         />
       </div>
     );
   }
 }
-function RenderTable(){
-  let context = 
-  [
+
+function RenderTable() {
+  const context = [
     "國立臺灣大學電機所 R10921A02 鍾麗文",
     "人生目前是爆炸的情況。",
     "以前這邊是放一些表演資訊什麼的，現在就是人生爆炸。",
-    "5/13 14可以來台大音樂節看羊駝樂團！",
+    "好希望這門課可以過關",
   ];
-  let lists = [];
-  for(let i=0;i<context.length;i++){
-    lists.push(<p id="word_hover" key={context[i]}>．{context[i]}</p>)
-  }
-  return lists  
-};
+
+  return (
+    <div>
+      {context.map((text) => (
+        <p id="word_hover" key={text}>．{text}</p>
+      ))}
+    </div>
+  );
+}
 
 function About() {
   return (
-    <div class="inner">
-        <div class ="flex flex-2">
-          <ChangeImg />
-          <div class="col col2">
-            <h3>About Me</h3>
-            <RenderTable />
-          </div>
+    <div className="inner">
+      <div className="flex flex-2">
+        <ChangeImg />
+        <div className="col col2">
+          <h3>About Me</h3>
+          <RenderTable />
         </div>
+      </div>
     </div>
   );
- 
 }
 
-
-export default About
+export default About;
